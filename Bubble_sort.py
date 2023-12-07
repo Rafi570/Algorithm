4065a7cdaccacbff38d19a35161279e95237c67b
HASAN RAFI AHMED <108696119+Rafi570@users.noreply.github.com>
2023-12-07 21:26:01 +0600

    Create Bubble_sort.py

---
+++ Bubble_sort.py
@@ -0,0 +1,14 @@
+class Bubble_sort:
+    def __int__(self,arr):
+        self.arr=arr
+
+    def sort_(self,array):
+        for i in range(len(array)-1):
+            for j in range(len(array)-i-1):
+                if array[j]>array[j+1]:
+                    array[j],array[j+1]=array[j+1],array[j]
+        return array
+if __name__ == "__main__":
+    array=[1,5,6,0,8,0,100,200]
+    ob=Bubble_sort()
+    print(ob.sort_(array))
