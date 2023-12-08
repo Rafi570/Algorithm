aa04f3c8f739041cc0c8e5dbc99bc2d693005cd9
HASAN RAFI AHMED <108696119+Rafi570@users.noreply.github.com>
2023-12-08 19:20:04 +0600

    Update Selection_Sort_Algorithm.py

--- Selection_Sort_Algorithm.py
+++ Selection_Sort_Algorithm.py
@@ -1,17 +1,17 @@
 class SelectionSort:
     def __init__(self,arr):
         self.arr=arr
-    def sort_(self,array):
-        for i in range(len(array)-1):
+    def sort_(self):
+        for i in range(len(arr)-1):
             mini=i
-            for j in range(i+1,len(array)):
-                if array[j] < array[mini]:
+            for j in range(i+1,len(arr)):
+                if arr[j] < arr[mini]:
                     mini = j
             if mini !=i:
-                array[i],array[mini]=array[mini],array[i]
-        return array
+                arr[i],arr[mini]=arr[mini],arr[i]
+        return arr
 
 if __name__ == "__main__":
     arr=[1,50,3,2,0,800,300]
     ob=SelectionSort(arr)
-    print(ob.sort_(arr))
+    print(ob.sort_())
